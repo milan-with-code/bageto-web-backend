110e12ba7b927965a90f31a3fb140251c256cc4c
milan-with-code <milan.code.github@gmail.com>
2025-08-23 20:19:50 +0530

    fix: prod url change

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://bageto.vercel.app"
+        "https://bageto.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
