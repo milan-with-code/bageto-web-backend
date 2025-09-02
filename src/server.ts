40eb619c95f26930c8eca97525f668da2f3f431c
milan-with-code <milan.code.github@gmail.com>
2025-09-02 22:27:43 +0530

    cros error

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import app from "./app";
 import { connectDB } from "./config/db";
-import { ENV } from "./config/env";
 
 connectDB();
 
-app.listen(ENV.PORT, () => {
-    console.log(`🚀 Server running on port ${ENV.PORT}`);
-});
+export default app;
